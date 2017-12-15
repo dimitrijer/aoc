eb3fa1b740b29dfd2568927406ff35556d6b28c4
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-12-15 20:38:12 +0100

    Add solution to eight puzzle

--- src/aoc/eight.clj
+++ src/aoc/eight.clj
@@ -8,21 +8,39 @@
               regid = #'[a-z]+'
               num = #'-?[0-9]+'
               op = 'inc' | 'dec'
-              cmp = '>' | '<' | '>=' | '<=' | '=='
+              cmp = '>' | '<' | '>=' | '<=' | '==' | '!='
               "
               :auto-whitespace :standard))
 
 (defn execute
-  [registers [_ [_ reg] [_ op] [_ operand] [_ cmp-reg] [_ cmp-op] [_ cmp-operand]]]
+  [registers [reg op operand-str cmp-reg cmp-op cmp-operand-str]]
   (let [cmp-reg-val (or (get registers cmp-reg) 0)
-        cmp-fn (if (= cmp-op "==") = (symbol cmp-op))
+        cmp-fn (case cmp-op
+                 "==" =
+                 "!=" not=
+                 (-> cmp-op symbol resolve))
+        cmp-operand (Integer/parseInt cmp-operand-str)
+        operand (Integer/parseInt operand-str)
         cond? (cmp-fn cmp-reg-val cmp-operand)
         op-fn (case op
                 "inc" #(+ (or % 0) operand)
-                "dec" #(- (or % 0) operand)
-                :default (throw (IllegalArgumentException.)))]
-    (update registers reg (if cond? op-fn identity) operand) ))
+                "dec" #(- (or % 0) operand))]
+    (update registers reg (if cond? op-fn identity))))
 
-(defn perform
-  [commands]
-  (reduce execute {} commands))
+(defn perform [commands] (reduce execute {} commands))
+
+(defn sanitize-commands
+  [dirty-commands]
+  (letfn [(sanitize-command [dirty-command]
+            (apply vector (map second (rest dirty-command))))]
+    (map sanitize-command (rest dirty-commands))))
+
+(defn solve
+  []
+  (->> (slurp "resources/eight.txt")
+       parser
+       sanitize-commands
+       perform
+       vals
+       (filter some?)
+       (apply max)))
